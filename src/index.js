70f6cd16c0a95d6b6a57b330d4676a591cbd057d
TC-harshshrivastava <harsh.shrivastava@techcarrel.com>
2026-01-22 19:34:04 +0530

    change theme of toast

--- src/index.js
+++ src/index.js
@@ -8,11 +8,39 @@ import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <BrowserRouter>
       <Provider store={store}>
         <App />
-        <Toaster/>
+        <Toaster
+           position="top-center"
+           reverseOrder={false}
+           toastOptions={{
+             duration: 3000,
+
+             style: {
+               background: '#1e293b',
+               color: '#fff',
+               marginTop: '60px',
+               border: '1px solid #334155',
+             },
+
+             success: {
+               iconTheme: {
+                 primary: '#22d3ee', 
+                 secondary: '#fff',
+               },
+             },
+
+             error: {
+               iconTheme: {
+                 primary: '#ef4444', 
+                 secondary: '#fff',
+               },
+             },
+           }}
+        />
       </Provider>
   </BrowserRouter>
 );
